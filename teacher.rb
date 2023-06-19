942c480d1569e54bd073b563d11d882fa7decee5
rosemutai <chepngetichrose2030@gmail.com>
2023-06-19 14:59:27 +0300

    Create Teacher class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require_Relative 'person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = "Unknown", parent_permission = true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+
+end
